2b1582a80fb1fbc91d9831bd595dc6c3b0dfce5f
foysalcodeio <md.foysalahammedforhad@gmail.com>
2025-03-24 20:44:31 +0600

    working on get api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://emaJohn:<db_password>@emajohn-cluster.qrt35.mongodb.net/?appName=emajohn-cluster";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@emajohn-cluster.qrt35.mongodb.net/?appName=emajohn-cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,11 +30,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const menuCollection = client.db("bistroDb").collection("menu");
+
+    app.get('/menu', async(req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
